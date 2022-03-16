1c3f546d8deca3792cd13da2437bd97cb61e6edc
Anurag Ajwani <anurag.ajwani@gmail.com>
2022-03-16 02:17:12 +0000

    Now generating xcodeproj with Tuist

---
+++ Project.swift
@@ -0,0 +1,27 @@
+import ProjectDescription
+let project = Project(
+    name: "SaladMaker",
+    organizationName: "com.anuragajwani",
+    targets: [
+        Target(
+            name: "SaladMaker",
+            platform: .iOS,
+            product: .app,
+            bundleId: "com.anuragajwani.SaladMaker",
+            deploymentTarget: .iOS(targetVersion: "14.0", devices: .iphone),
+            infoPlist: "SaladMaker/Info.plist",
+            sources: ["SaladMaker/**"],
+            resources: ["SaladMaker/Assets.xcassets/"],
+            dependencies: [.target(name: "SayHelloKit")]
+        ),
+        Target(
+            name: "SayHelloKit",
+            platform: .iOS,
+            product: .framework,
+            bundleId: "com.anuragajwani.SayHelloKit",
+            deploymentTarget: .iOS(targetVersion: "14.0", devices: .iphone),
+            sources: ["SayHelloKit/**"],
+            settings: .settings(base: ["GENERATE_INFOPLIST_FILE": "YES"])
+        )
+    ]
+)
